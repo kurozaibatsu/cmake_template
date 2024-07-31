b039228390d3a70ebf62d97fdecc062689d14f99
kurozaibatsu <raphael.lamp@gmail.com>
2024-07-31 12:35:28 +0200

    use GTest

--- myapp/third_party/mylib/test/test.cpp
+++ myapp/third_party/mylib/test/test.cpp
@@ -1,12 +1,16 @@
 #include "module_a/module_ab.hpp"
 #include <cstdio>
-#include <string>
+#include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
-  printf("argc: %d\n", argc);
-  if (2 > argc) {
-    return 0;
-  }
-  auto result = module_a::do_something(std::stoi(argv[1]));
-  printf("%d\n", result);
+// Demonstrate some basic assertions.
+TEST(mylib, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(mylib, do_something) {
+  EXPECT_EQ(1, module_a::do_something(0));
+  EXPECT_EQ(0, module_a::do_something(-1));
 }
